edce7922bbf8fd7161ffec68707e1b52801783b1
andres ramirez <gandres.ramirez@gmail.com>
2017-03-14 12:15:54 -0500

    Docs

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,10 @@
 const path = require('path');
+
 module.exports = {
     entry: ['./app/index.js'],
     output: {
-      path: path.resolve(__dirname, './build'),
-      filename: 'bundle.js'
+      path: path.resolve(__dirname, 'build'),
+      filename: 'bundlee.js'
     },
     module: {
       rules: [{
